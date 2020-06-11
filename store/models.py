0396b51fa9813a34f240828941e1905373f6061b
atseplyaev <sanjok126@yandex.ru>
2020-06-11 08:23:31 +0300

    add product fields: image url and descriptions
    Signed-off-by: atseplyaev <sanjok126@yandex.ru>

--- store/models.py
+++ store/models.py
@@ -5,6 +5,8 @@ db = SQLAlchemy()
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.Text)
+    description = db.Column(db.String(255))
     name = db.Column(db.String(80), unique=True, nullable=False)
     display_size = db.Column(db.Float, nullable=False)
     cpu = db.Column(db.String(32), nullable=False)
